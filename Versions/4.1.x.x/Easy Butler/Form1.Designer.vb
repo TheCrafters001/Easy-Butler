e7c65f6b2de9a361bbe005756e1d0de686e3a7aa
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2019-09-27 17:13:15 -0400

    Working on Metro Design

--- Versions/4.1.x.x/Easy Butler/Form1.Designer.vb
+++ Versions/4.1.x.x/Easy Butler/Form1.Designer.vb
@@ -22,66 +22,197 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.MetroLabel1 = New MetroFramework.Controls.MetroLabel()
         Me.MetroButton1 = New MetroFramework.Controls.MetroButton()
         Me.MetroButton2 = New MetroFramework.Controls.MetroButton()
         Me.MetroButton3 = New MetroFramework.Controls.MetroButton()
         Me.MetroButton4 = New MetroFramework.Controls.MetroButton()
+        Me.MetroTextBox1 = New MetroFramework.Controls.MetroTextBox()
+        Me.MetroTextBox2 = New MetroFramework.Controls.MetroTextBox()
+        Me.MetroLabel2 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroTextBox3 = New MetroFramework.Controls.MetroTextBox()
+        Me.MetroLabel3 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroComboBox1 = New MetroFramework.Controls.MetroComboBox()
+        Me.MetroLabel4 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroTextBox4 = New MetroFramework.Controls.MetroTextBox()
+        Me.MetroLabel5 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroButton5 = New MetroFramework.Controls.MetroButton()
         Me.SuspendLayout()
         '
         'MetroLabel1
         '
         Me.MetroLabel1.AutoSize = True
-        Me.MetroLabel1.Location = New System.Drawing.Point(23, 89)
+        Me.MetroLabel1.Location = New System.Drawing.Point(23, 96)
         Me.MetroLabel1.Name = "MetroLabel1"
-        Me.MetroLabel1.Size = New System.Drawing.Size(81, 19)
+        Me.MetroLabel1.Size = New System.Drawing.Size(71, 19)
+        Me.MetroLabel1.Style = MetroFramework.MetroColorStyle.Green
         Me.MetroLabel1.TabIndex = 0
-        Me.MetroLabel1.Text = "MetroLabel1"
+        Me.MetroLabel1.Text = "Username:"
+        Me.MetroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark
         '
         'MetroButton1
         '
         Me.MetroButton1.Location = New System.Drawing.Point(23, 63)
         Me.MetroButton1.Name = "MetroButton1"
         Me.MetroButton1.Size = New System.Drawing.Size(114, 23)
+        Me.MetroButton1.Style = MetroFramework.MetroColorStyle.Green
         Me.MetroButton1.TabIndex = 1
         Me.MetroButton1.Text = "Login to Itch.IO"
+        Me.MetroButton1.Theme = MetroFramework.MetroThemeStyle.Dark
         '
         'MetroButton2
         '
         Me.MetroButton2.Location = New System.Drawing.Point(143, 63)
         Me.MetroButton2.Name = "MetroButton2"
         Me.MetroButton2.Size = New System.Drawing.Size(116, 23)
+        Me.MetroButton2.Style = MetroFramework.MetroColorStyle.Green
         Me.MetroButton2.TabIndex = 2
         Me.MetroButton2.Text = "Update Butler"
+        Me.MetroButton2.Theme = MetroFramework.MetroThemeStyle.Dark
         '
         'MetroButton3
         '
         Me.MetroButton3.Location = New System.Drawing.Point(265, 63)
         Me.MetroButton3.Name = "MetroButton3"
         Me.MetroButton3.Size = New System.Drawing.Size(75, 23)
+        Me.MetroButton3.Style = MetroFramework.MetroColorStyle.Green
         Me.MetroButton3.TabIndex = 3
         Me.MetroButton3.Text = "About"
+        Me.MetroButton3.Theme = MetroFramework.MetroThemeStyle.Dark
         '
         'MetroButton4
         '
         Me.MetroButton4.Location = New System.Drawing.Point(346, 63)
         Me.MetroButton4.Name = "MetroButton4"
-        Me.MetroButton4.Size = New System.Drawing.Size(103, 23)
+        Me.MetroButton4.Size = New System.Drawing.Size(105, 23)
+        Me.MetroButton4.Style = MetroFramework.MetroColorStyle.Green
         Me.MetroButton4.TabIndex = 4
         Me.MetroButton4.Text = "Check for Update"
+        Me.MetroButton4.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'MetroTextBox1
+        '
+        Me.MetroTextBox1.Location = New System.Drawing.Point(100, 92)
+        Me.MetroTextBox1.Name = "MetroTextBox1"
+        Me.MetroTextBox1.Size = New System.Drawing.Size(351, 23)
+        Me.MetroTextBox1.TabIndex = 5
+        Me.MetroTextBox1.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'MetroTextBox2
+        '
+        Me.MetroTextBox2.Location = New System.Drawing.Point(103, 121)
+        Me.MetroTextBox2.Name = "MetroTextBox2"
+        Me.MetroTextBox2.Size = New System.Drawing.Size(348, 23)
+        Me.MetroTextBox2.TabIndex = 7
+        Me.MetroTextBox2.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'MetroLabel2
+        '
+        Me.MetroLabel2.AutoSize = True
+        Me.MetroLabel2.Location = New System.Drawing.Point(23, 125)
+        Me.MetroLabel2.Name = "MetroLabel2"
+        Me.MetroLabel2.Size = New System.Drawing.Size(74, 19)
+        Me.MetroLabel2.Style = MetroFramework.MetroColorStyle.Green
+        Me.MetroLabel2.TabIndex = 6
+        Me.MetroLabel2.Text = "Game URL:"
+        Me.MetroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'MetroTextBox3
+        '
+        Me.MetroTextBox3.Location = New System.Drawing.Point(80, 150)
+        Me.MetroTextBox3.Name = "MetroTextBox3"
+        Me.MetroTextBox3.Size = New System.Drawing.Size(315, 23)
+        Me.MetroTextBox3.TabIndex = 30
+        Me.MetroTextBox3.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'MetroLabel3
+        '
+        Me.MetroLabel3.AutoSize = True
+        Me.MetroLabel3.Location = New System.Drawing.Point(23, 154)
+        Me.MetroLabel3.Name = "MetroLabel3"
+        Me.MetroLabel3.Size = New System.Drawing.Size(51, 19)
+        Me.MetroLabel3.Style = MetroFramework.MetroColorStyle.Green
+        Me.MetroLabel3.TabIndex = 29
+        Me.MetroLabel3.Text = "Folder:"
+        Me.MetroLabel3.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'MetroComboBox1
+        '
+        Me.MetroComboBox1.FormattingEnabled = True
+        Me.MetroComboBox1.ItemHeight = 23
+        Me.MetroComboBox1.Items.AddRange(New Object() {"windows", "linux", "mac", "android"})
+        Me.MetroComboBox1.Location = New System.Drawing.Point(59, 179)
+        Me.MetroComboBox1.Name = "MetroComboBox1"
+        Me.MetroComboBox1.Size = New System.Drawing.Size(392, 29)
+        Me.MetroComboBox1.TabIndex = 31
+        '
+        'MetroLabel4
+        '
+        Me.MetroLabel4.AutoSize = True
+        Me.MetroLabel4.Location = New System.Drawing.Point(23, 189)
+        Me.MetroLabel4.Name = "MetroLabel4"
+        Me.MetroLabel4.Size = New System.Drawing.Size(30, 19)
+        Me.MetroLabel4.Style = MetroFramework.MetroColorStyle.Green
+        Me.MetroLabel4.TabIndex = 32
+        Me.MetroLabel4.Text = "OS:"
+        Me.MetroLabel4.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'MetroTextBox4
+        '
+        Me.MetroTextBox4.Location = New System.Drawing.Point(80, 218)
+        Me.MetroTextBox4.Name = "MetroTextBox4"
+        Me.MetroTextBox4.Size = New System.Drawing.Size(371, 23)
+        Me.MetroTextBox4.TabIndex = 34
+        Me.MetroTextBox4.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'MetroLabel5
+        '
+        Me.MetroLabel5.AutoSize = True
+        Me.MetroLabel5.Location = New System.Drawing.Point(23, 218)
+        Me.MetroLabel5.Name = "MetroLabel5"
+        Me.MetroLabel5.Size = New System.Drawing.Size(54, 19)
+        Me.MetroLabel5.Style = MetroFramework.MetroColorStyle.Green
+        Me.MetroLabel5.TabIndex = 33
+        Me.MetroLabel5.Text = "Version:"
+        Me.MetroLabel5.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'MetroButton5
+        '
+        Me.MetroButton5.Location = New System.Drawing.Point(403, 150)
+        Me.MetroButton5.Name = "MetroButton5"
+        Me.MetroButton5.Size = New System.Drawing.Size(48, 23)
+        Me.MetroButton5.TabIndex = 35
+        Me.MetroButton5.Text = "Browse"
+        Me.MetroButton5.Theme = MetroFramework.MetroThemeStyle.Dark
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(474, 325)
+        Me.Controls.Add(Me.MetroButton5)
+        Me.Controls.Add(Me.MetroTextBox4)
+        Me.Controls.Add(Me.MetroLabel5)
+        Me.Controls.Add(Me.MetroLabel4)
+        Me.Controls.Add(Me.MetroComboBox1)
+        Me.Controls.Add(Me.MetroTextBox3)
+        Me.Controls.Add(Me.MetroLabel3)
+        Me.Controls.Add(Me.MetroTextBox2)
+        Me.Controls.Add(Me.MetroLabel2)
+        Me.Controls.Add(Me.MetroTextBox1)
         Me.Controls.Add(Me.MetroButton4)
         Me.Controls.Add(Me.MetroButton3)
         Me.Controls.Add(Me.MetroButton2)
         Me.Controls.Add(Me.MetroButton1)
         Me.Controls.Add(Me.MetroLabel1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
         Me.Name = "Form1"
+        Me.Resizable = False
+        Me.Style = MetroFramework.MetroColorStyle.Green
         Me.Text = "Easy Butler"
+        Me.Theme = MetroFramework.MetroThemeStyle.Dark
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -92,4 +223,14 @@ Partial Class Form1
     Friend WithEvents MetroButton2 As MetroFramework.Controls.MetroButton
     Friend WithEvents MetroButton3 As MetroFramework.Controls.MetroButton
     Friend WithEvents MetroButton4 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroTextBox1 As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents MetroTextBox2 As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents MetroLabel2 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroTextBox3 As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents MetroLabel3 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroComboBox1 As MetroFramework.Controls.MetroComboBox
+    Friend WithEvents MetroLabel4 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroTextBox4 As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents MetroLabel5 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroButton5 As MetroFramework.Controls.MetroButton
 End Class
--- Versions/4.1.x.x/Easy Butler/Form1.vb
+++ Versions/4.1.x.x/Easy Butler/Form1.vb
@@ -1,6 +1,9 @@
 ﻿Imports AutoUpdaterDotNET
 
 Public Class Form1
+
+    Dim butler As String = "butler.exe" ' This will be used to check for butler.
+    Dim FilenameUpdate As String = System.IO.Path.GetFileName(butler) ' This will be used to find butler in the file system.
     Private Sub MetroButton4_Click(sender As Object, e As EventArgs) Handles MetroButton4.Click
         UpdateCheck()
     End Sub
@@ -16,5 +19,36 @@ Public Class Form1
         AutoUpdater.DownloadPath = Environment.CurrentDirectory
 
         AutoUpdater.Start("http://thecrafters001.ga/EasyButler.xml")
+    End Sub
+
+    Private Sub MetroButton1_Click(sender As Object, e As EventArgs) Handles MetroButton1.Click
+        If System.IO.File.Exists(butler) Then
+            Dim pHelp As New ProcessStartInfo
+            pHelp.FileName = "butler.exe"
+            pHelp.Arguments = "login"
+            pHelp.UseShellExecute = True
+            pHelp.WindowStyle = ProcessWindowStyle.Normal
+            Dim proc As Process = Process.Start(pHelp)
+        Else
+            MessageBox.Show("Failed to login. Butler is missing!", "Error: File Missing", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+    End Sub
+
+    Private Sub MetroButton2_Click(sender As Object, e As EventArgs) Handles MetroButton2.Click
+        If System.IO.File.Exists(butler) Then
+            Dim pHelp As New ProcessStartInfo
+            pHelp.FileName = "butler.exe"
+            pHelp.Arguments = "update"
+            pHelp.UseShellExecute = True
+            pHelp.WindowStyle = ProcessWindowStyle.Normal
+            Dim proc As Process = Process.Start(pHelp)
+        Else
+            MessageBox.Show("Failed to update. Butler is missing!", "Error: File Missing", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub MetroButton3_Click(sender As Object, e As EventArgs) Handles MetroButton3.Click
+        AboutBox1.Show()
     End Sub
 End Class
